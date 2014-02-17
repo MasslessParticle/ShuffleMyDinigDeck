aa1a40b285843dc86c304389fd690230caa29f83
Travis Patterson <massless@unm.edu>
2014-02-17 12:43:11 -0700

    Applied GPL

--- diningdeck/admin.py
+++ diningdeck/admin.py
@@ -1,12 +1,25 @@
+'''
+This file is part of Shuffle My Dining Deck.
+
+Shuffle My Dining Deck is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Shuffle My Dining Deck is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+Author: Travis Patterson (masslessparticle@gmail.com)
+'''
+
 from django.contrib import admin
-from django.contrib.auth.models import User
 
 from diningdeck.models import Restaurant, UserEaten
-# Register your models here.
-
-
-#class UserInline(admin.TabularInline):
-#    model = User
 
 class RestaurantAdmin(admin.ModelAdmin):
     search_fields = ['restaurant']
--- diningdeck/views.py
+++ diningdeck/views.py
@@ -1,5 +1,23 @@
+'''
+This file is part of Shuffle My Dining Deck.
+
+Shuffle My Dining Deck is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Shuffle My Dining Deck is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+Author: Travis Patterson (masslessparticle@gmail.com)
+'''
+
 from random import shuffle
-from urllib import unquote
 
 from django.shortcuts import render, redirect
 from django.core.context_processors import csrf
